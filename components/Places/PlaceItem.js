3b4caebc9dd1d5588e4b20353c2ff92379f5441c
Yuli Rumahorbo <yulirumahorbo.dev@gmail.com>
2025-03-04 17:57:20 +0700

    init: Initialize project

---
+++ components/Places/PlaceItem.js
@@ -0,0 +1,17 @@
+import { Pressable, View, Image, Text, StyleSheet } from "react-native";
+
+function PlaceItem({ place, onSelect }) {
+  return (
+    <Pressable onPress={onSelect}>
+      <Image source={{ uri: place.imageUri }} />
+      <View>
+        <Text>{place.title}</Text>
+        <Text>{place.address}</Text>
+      </View>
+    </Pressable>
+  );
+}
+
+export default PlaceItem;
+
+const styles = StyleSheet.create({});
