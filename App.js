b255aef46988a939a169f26730ea688ca68eb679
Yuli Rumahorbo <yulirumahorbo.dev@gmail.com>
2025-03-05 09:52:41 +0700

    feat: Adding a header button & global colors

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import { StatusBar } from "expo-status-bar";
 
 import AllPlaces from "./screens/AllPlaces";
 import AddPlace from "./screens/AddPlace";
+import IconButton from "./components/Ui/IconButton";
+import { Colors } from "./constants.js/color";
 
 const Stack = createNativeStackNavigator();
 
@@ -12,9 +14,37 @@ export default function App() {
     <>
       <StatusBar style="dark" />
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="AllPlaces" component={AllPlaces} />
-          <Stack.Screen name="AddPlace" component={AddPlace} />
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: Colors.primary500 },
+            headerTintColor: Colors.gray700,
+            contentStyle: {
+              backgroundColor: Colors.gray700,
+            },
+          }}
+        >
+          <Stack.Screen
+            name="AllPlaces"
+            component={AllPlaces}
+            options={({ navigation }) => ({
+              title: "Your Favorite Places",
+              headerRight: ({ tintColor }) => (
+                <IconButton
+                  icon="add"
+                  size={24}
+                  color={tintColor}
+                  onPress={() => navigation.navigate("AddPlace")}
+                />
+              ),
+            })}
+          />
+          <Stack.Screen
+            name="AddPlace"
+            component={AddPlace}
+            options={{
+              title: "Add a new Place",
+            }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
