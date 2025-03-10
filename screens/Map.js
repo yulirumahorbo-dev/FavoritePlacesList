93e071c9a0365105e7a3ccf1a74977c3eb49d9ff
Yuli Rumahorbo <yulirumahorbo.dev@gmail.com>
2025-03-10 12:30:53 +0700

    feat: Allowing map interaction & adding markers

--- screens/Map.js
+++ screens/Map.js
@@ -1,14 +1,45 @@
+import { useState } from "react";
 import { StyleSheet } from "react-native";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 
 function Map() {
+  const [selectedLocation, setSelectedLocation] = useState();
+
   const region = {
     latitude: 3.56607470645906,
     longitude: 98.87483736149491,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   };
-  return <MapView style={styles.map} initialRegion={region} />;
+
+  function selectLocationHandler(event) {
+    console.log(event);
+    const lat = event.nativeEvent.coordinate.latitude;
+    const lng = event.nativeEvent.coordinate.longitude;
+
+    setSelectedLocation({
+      lat: lat,
+      lng: lng,
+    });
+  }
+
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={region}
+      onPress={selectLocationHandler}
+    >
+      {selectedLocation && (
+        <Marker
+          title="Picked Location"
+          coordinate={{
+            latitude: selectedLocation.lat,
+            longitude: selectedLocation.lng,
+          }}
+        />
+      )}
+    </MapView>
+  );
 }
 
 export default Map;
