10228a9f5c3c5c5feb87911637d67cb9703129df
Yuli Rumahorbo <yulirumahorbo.dev@gmail.com>
2025-03-14 13:21:03 +0700

    feat: SQLite: getting started & initialization

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
-
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect, useState, useCallback } from "react";
 import AllPlaces from "./screens/AllPlaces";
 import AddPlace from "./screens/AddPlace";
 import IconButton from "./components/Ui/IconButton";
 import Map from "./screens/Map";
 import { Colors } from "./constants/colors";
+import { init } from "./util/database";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [dbInitialized, setDbInitialized] = useState(false);
+
+  useEffect(() => {
+    init()
+      .then(() => {
+        setDbInitialized(true);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (dbInitialized) {
+      await SplashScreen.hideAsync();
+    }
+  }, [dbInitialized]);
+
+  if (!dbInitialized) return null;
+
   return (
     <>
       <StatusBar style="dark" />
-      <NavigationContainer>
+      <NavigationContainer onReady={onLayoutRootView}>
         <Stack.Navigator
           screenOptions={{
             headerStyle: { backgroundColor: Colors.primary500 },
---
+++ util/database.js
@@ -0,0 +1,16 @@
+import * as SQLite from "expo-sqlite";
+
+const database = SQLite.openDatabaseSync("places.db");
+
+export function init() {
+  return database.runAsync(`
+        CREATE TABLE IF NOT EXISTS places (
+            id INTEGER PRIMARY KEY NOT NULL,
+            title TEXT NOT NULL,
+            imageUri TEXT NOT NULL,
+            address TEXT NOT NULL,
+            lat REAL NOT NULL,
+            lng REAL NOT NULL
+        )
+    `);
+}
